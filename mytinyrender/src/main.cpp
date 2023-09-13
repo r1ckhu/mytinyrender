1189ba14ef296ecfd6c07e34f9b4e554e272a428
Rickhu <rickhu123@outlook.com>
2023-09-13 17:20:38 +0800

    feat: complete phone shading
    Complete Lesson 6: Shaders for the software renderer in tinyrender wiki.

--- mytinyrender/src/main.cpp
+++ mytinyrender/src/main.cpp
@@ -12,9 +12,15 @@ const TGAColor blue = TGAColor{ 0,0,255,255 };
 const std::string DEFAULT_OBJ_PATH = std::string("obj/african_head/african_head.obj");
 const int IMG_WIDTH = 1200;
 const int IMG_HEIGHT = 1200;
-Vec3f light_dir = { 0,0,-1 };
 std::unique_ptr<Model> model;
 std::unique_ptr<std::vector<float>> z_buffer;
+
+Vec3f light_dir = { -1,-1,-1 };
+Vec3f camera_pos = { 0,0,3 };
+Vec3f look_at = { 0,0,-1 };
+Vec3f up = { 0,1,0 }; // prep to look_at
+Vec2f nf = { -2.5,-4 };
+float fovY_deg = 45;
 
 struct gouraud_shader_t : shader_t
 {
@@ -92,6 +98,34 @@ struct normal_mapping_shader_t : shader_t
 	}
 };
 
+struct phone_shader_t : shader_t
+{
+	mat<2, 3, float> varying_uv;
+	mat<4, 4, float> uniform_M;   //  Projection*View
+	mat<4, 4, float> uniform_MIT; // (Projection*View).invert_transpose()
+
+	Vec4f vertex(int iface, int nthvert) final {
+		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+		Vec4f gl_vertex = vec3f_to_vec4f(model->vert(iface, nthvert));
+		gl_vertex = viewport * projection * view * gl_vertex;
+		gl_vertex = gl_vertex / gl_vertex[3];
+		return gl_vertex;
+	}
+	bool fragment(Vec3f bar, TGAColor& color) final {
+		Vec2f uv = varying_uv * bar;
+		Vec3f n = vec4f_to_vec3f(uniform_MIT * vec3f_to_vec4f(model->normal(uv))).normalize();
+		Vec3f l = vec4f_to_vec3f(uniform_M * vec3f_to_vec4f(light_dir)).normalize();
+		Vec3f h = vec4f_to_vec3f(uniform_M * vec3f_to_vec4f((light_dir + look_at) * -1)).normalize();
+		float specular = std::pow(std::max(0.f, n * h),model->specular(uv));
+		float diffuse = std::max(0.f, n * l * -1);
+		color = model->diffuse(uv);
+		for (int i = 0; i < 3; i++) {
+			color[i] = std::min<float>(5 + color[i] * (diffuse + .8 * specular), 255);
+		}
+		return false;                              // no, we do not discard this pixel
+	}
+};
+
 int main(int argc, char** argv)
 {
 	if (argc == 2) {
@@ -103,15 +137,9 @@ int main(int argc, char** argv)
 	TGAImage image{ IMG_WIDTH,IMG_HEIGHT,TGAImage::RGB };
 	z_buffer = std::make_unique<std::vector<float>>(IMG_WIDTH * IMG_HEIGHT, std::numeric_limits<float>::lowest());
 
-	Vec3f camera_pos = { 0,0,3 };
-	Vec3f look_at = { 0,0,-1 };
-	Vec3f up = { 0,1,0 }; // prep to look_at
-	Vec2f nf = { -2.5,-4 };
-	float fovY_deg = 45;
-
 	init_camera(camera_pos, look_at, up, fovY_deg, nf, Vec2f{ 1, 1 }, Vec2i{ IMG_WIDTH, IMG_HEIGHT }, true);
 
-	normal_mapping_shader_t shader;
+	phone_shader_t shader;
 	shader.uniform_M = projection * view;
 	shader.uniform_MIT = shader.uniform_M.invert_transpose();
 	// toon_shader_t shader;
